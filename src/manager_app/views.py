db9cc1bc37e72b2497afc157260dcad4855d3e22
homirun <hatunemiku.moco@gmail.com>
2020-04-19 04:06:20 +0900

    構成をいい感じにした

--- src/manager_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-# Create your views here.
-
-
-class IndexView(TemplateView):
-    template_name = "manager_app/index.html"
